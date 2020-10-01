c52da0d53a7c2e39407768a32a5be3c705147353
AikaOnimura <aika.onimura@gmail.com>
2020-10-01 06:05:49 +0000

    [Add]4-9 新規登録画面・ログイン画面の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    #新規登録後飛ぶかを指定
+    def after_sign_in_path_for(resource)
+        homes_top_path
+    end
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    end
+
 end

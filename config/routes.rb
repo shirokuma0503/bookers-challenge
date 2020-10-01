6871b817426e5cf572bb0e3af5cf2f1739677a80
AikaOnimura <aika.onimura@gmail.com>
2020-10-01 05:19:27 +0000

    [Add]4-9 devisegemをインストール、Userモデル作成、カラムの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'homes/top' #全体のトップ画面表示
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

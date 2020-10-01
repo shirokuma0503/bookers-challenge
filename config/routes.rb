01397fd6d088d7dfee33b1fd995a9215f41bfb78
AikaOnimura <aika.onimura@gmail.com>
2020-10-01 06:13:16 +0000

    [Add]4-9 トップ画面を/comで表示させる

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'homes/top' #全体のトップ画面表示
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    devise_for :users
+    root to: 'homes#top' #全体のトップ画面を/comで表示
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

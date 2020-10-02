c79fdde3287ef2ffe8a2fdb21ee7ff0cb4a2f883
AikaOnimura <aika.onimura@gmail.com>
2020-10-02 13:38:46 +0000

    [Add]4-9 Userモデルの作成、usersコントローラの作成、ルーティングの追加、コントローラにアクションを定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,17 @@
 class BooksController < ApplicationController
- 
+  
   def create
   end
- 
+  
   def index
   end
 
   def show
   end
-
+  
   def edit
   end
-  
+
   def update
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_121731) do
+ActiveRecord::Schema.define(version: 2020_10_02_132638) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"

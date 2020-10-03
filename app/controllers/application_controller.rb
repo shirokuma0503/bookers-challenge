ae029d23e2ec7407e4a2e1bd1b0bb2146bc1321d
AikaOnimura <aika.onimura@gmail.com>
2020-10-03 12:00:16 +0000

    [Add]4-9 application.htmlに共通のHTMLを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     #新規登録後マイページへとぶ
-    def after_sign_up_path_for(resource)
-        user_path(resource)
-    end
+
     #ログイン後マイページへとぶ
     def after_sign_in_path_for(resource)
         user_path(resource)

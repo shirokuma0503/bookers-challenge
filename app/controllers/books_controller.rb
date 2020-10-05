716a03fe7924815e6cee6deaaca0156b1e0688b3
AikaOnimura <aika.onimura@gmail.com>
2020-10-05 06:07:12 +0000

    [Add]4-9 投稿編集画面のHTMLを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,15 +21,24 @@ class BooksController < ApplicationController
   def show
       @book = Book.find(params[:id])
       @book_new = Book.new
+      @user = @book.user
   end
 
   def edit
+      @book = Book.find(params[:id])
   end
 
   def update
+      @book = Book.find(params[:id])
+      @book.id = current_user.id
+      @book.update(book_params)
+      redirect_to book_path(@book)
   end
 
   def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
   end
 
   private

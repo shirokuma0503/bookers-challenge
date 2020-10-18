f5959c381344679d176611ad18545369b5fd9a97
EC2 Default User <ec2-user@ip-172-31-36-18.ec2.internal>
2020-10-18 07:06:01 +0000

    [Add]応用課題3/投稿一覧・投稿詳細画面にコメント数を表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_132638) do
+ActiveRecord::Schema.define(version: 2020_10_18_064001) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"

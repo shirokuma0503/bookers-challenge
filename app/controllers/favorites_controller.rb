5cabc645da1251b8a45ba479fc77c86968e47da6
EC2 Default User <ec2-user@ip-172-31-36-18.ec2.internal>
2020-10-19 04:52:55 +0000

    [Add]応用課題3/いいね機能の実装、リダイレクトの便利な書き方を教わったので追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,22 +5,14 @@ class FavoritesController < ApplicationController
       book = Book.find(params[:book_id])
       favorite = current_user.favorites.new(book_id: book.id)
       favorite.save
-      if params[:from_page] == "index"
-      redirect_to books_path
-      else
-        redirect_to book_path(book.id)
-      end
+      redirect_to request.referer
   end
 
   def destroy
       book = Book.find(params[:book_id])
       favorite = current_user.favorites.find_by(book_id: book.id)
       favorite.destroy
-      if params[:from_page] == "index"
-      redirect_to books_path
-      else
-        redirect_to book_path(book.id)
-      end
+      redirect_to request.referer
   end
 
 end

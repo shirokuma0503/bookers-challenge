97261fe38834e0105b86d5b808eb60d4167c0669
EC2 Default User <ec2-user@ip-172-31-36-18.ec2.internal>
2020-10-20 11:13:38 +0000

    [Fix/Add]応用課題5/条件検索ができるようにコントローラを修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,16 +6,28 @@ class SearchController < ApplicationController
         @method = params["search"]["method"]
         @records = search_for(@model,@content,@method)
     end
-    
+
     private
-    
+
     def search_for(model,content,method)
         if model == 'user'
             if method == 'perfect_match'
                User.where('name LIKE ?', '%'+content+'%')
+            elsif method == 'forward_match'
+               User.where('name LIKE ?', '%'+content)
+            elsif method == 'backward_match'
+               User.where('name LIKE ?', content+'%')
+            elsif method == 'partial'
+               User.where('name LIKE ?', '%'+content+'%')
             end
         else
             if method == 'perfect_match'
+               Book.where('title LIKE ?', '%'+content+'%')
+            elsif method == 'forward_match'
+               Book.where('title LIKE ?', '%'+content)
+            elsif method == 'backward_match'
+               Book.where('title LIKE ?', content+'%')
+            elsif method == 'partial'
                Book.where('title LIKE ?', '%'+content+'%')
             end
         end

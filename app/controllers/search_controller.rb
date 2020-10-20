aabb715d0a103be43b7076335193c88d734b5aa8
EC2 Default User <ec2-user@ip-172-31-36-18.ec2.internal>
2020-10-20 12:14:39 +0000

    [Add]応用課題5/検索機能の実装、コメントの追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,20 +4,20 @@ class SearchController < ApplicationController
         @model = params["search"]["model"]
         @content = params["search"]["content"]
         @method = params["search"]["method"]
-        @records = search_for(@model,@content,@method)
+        @records = search_for(@model,@content,@method) #@user@bookの情報が入っている
     end
 
     private
 
     def search_for(model,content,method)
         if model == 'user'
-            if method == 'perfect_match'
-               User.where('name LIKE ?', '%'+content+'%')
-            elsif method == 'forward_match'
+            if method == 'perfect_match' #完全一致
+               User.where('name LIKE ?', '%'+content+'%') #モデル名.where('カラム名 LIKE?', 条件に合わせた中身)
+            elsif method == 'forward_match' #前方一致
                User.where('name LIKE ?', '%'+content)
-            elsif method == 'backward_match'
+            elsif method == 'backward_match' #後方一致
                User.where('name LIKE ?', content+'%')
-            elsif method == 'partial'
+            elsif method == 'partial' #部分一致
                User.where('name LIKE ?', '%'+content+'%')
             end
         else
